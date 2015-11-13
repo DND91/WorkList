284f34fa4d30f8403c148638d3760ed79f143a75
DND91 <Jonathan.Crusoe@Gmail.com>
2015-11-13 22:29:35 +0100

    test

--- worklist.lua
+++ worklist.lua
@@ -7,9 +7,19 @@ local monitor = peripheral.wrap("left")
 local running = true
 local str = ""
 
+function keyToChar(key)
+  if key == keys.enter then
+    return "\n"
+   else if key == keys.space then
+     return " "
+   else
+     return keys.getName(key)
+   end
+end
+
 while running do
   local event, key, isHeld = os.pullEvent("key")
-  str = str .. keys.getName( key )
+  str = str .. keyToChar( key )
   monitor.clear()
   monitor.setCursorPos(1,1)
   monitor.write(str)
@@ -18,6 +28,7 @@ end
 
 -- PRINT FUNCTION
 -- INPUT FUNCTION
+-- INPUT TO CHAR CONVERTER
 -- MAIN LOOP WITH MENU
 -- SAVE/LOAD FILE; WORKLIST TABLE
 -- BUTTON HANDLER FUNCTION
