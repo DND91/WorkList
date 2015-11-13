7380d66764677665fdc28ac1a8199533755f4aa6
DND91 <Jonathan.Crusoe@Gmail.com>
2015-11-13 22:14:53 +0100

    test

--- worklist.lua
+++ worklist.lua
@@ -4,14 +4,22 @@
 
 
 local monitor = peripheral.wrap("left")
-monitor.clear()
-peripheral.wrap("SIDE").setCursorPos(1,1)
-monitor.write( "Hello World!" )
 
 
+local running = true
+while running do
+  local event, str = os.pullEvent( "char" ) -- limit os.pullEvent to the 'key' event
+  monitor.clear()
+  monitor.setCursorPos(1,1)
+  monitor.write(str)
+end
 
 
-
+-- PRINT FUNCTION
+-- INPUT FUNCTION
+-- MAIN LOOP WITH MENU
+-- SAVE/LOAD FILE; WORKLIST TABLE
+-- BUTTON HANDLER FUNCTION
 
 
 
